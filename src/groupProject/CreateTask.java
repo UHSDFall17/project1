ee1dab63622504f412727b1a1f0149779799126a
Keith Ng <climbr39696@gmail.com>
2017-11-16 16:16:30 -0600

    Fixed errors and added new functions

--- src/groupProject/CreateTask.java
+++ src/groupProject/CreateTask.java
@@ -2,25 +2,30 @@ package com.group1.project1.anyDont;
 
 import java.util.Scanner;
 import java.util.Date;
-import java.util.Calender;
+import java.util.Calendar;
 import java.text.SimpleDateFormat;
 
 public class CreateTask {
 	
 	Scanner sc = new Scanner(System.in);
-	private String note;
-	private String description;
-	private int year;
+	private String note = null;
+	private String description = null;
+	private int year = 1;
 	private int month;
 	private int day;
+	private int hour = 0;
+	private int minute = 0;
+	
 	private String date;
+	private String timeDue;
 	private SimpleDateFormat sdf = new SimpleDateFormat("MM-dd-yyyy");
+	private SimpleDateFormat sdf2 = new SimpleDateFormat("HH:mm");
 
-	private bool isRepeating = false;
-	private bool isCompleted = false;
+	private boolean isRepeating = false;
+	private boolean isCompleted = false;
 	
 	
-	public string getTask()
+	public String getTask()
 	{
 		System.out.print("Enter the description of your task: ");
 		String description = sc.nextLine();
@@ -28,7 +33,7 @@ public class CreateTask {
 		return description;
 	}
 	
-		public String getDescription()
+	public String getDescription()
 	{
 		String note = sc.nextLine();
 		return note;
@@ -47,29 +52,46 @@ public class CreateTask {
 		return month;
 	}
 		
-	public int getDay(int month)
+	public int getDay(int year, int month)
 	{
-		if(month = 2)
+		if(month == 2)
 		{
-			System.out.print("Day(1-28): ");
-			day = sc.nextInt();
-			while(day < 1 || day > 28)
+			if(isLeapYear(year) == false)
 			{
-					System.err.println("Input is out of range");
-					System.out.print("Day(1-28): ");
-					day = sc.nextInt();
+				System.out.print("Day(1-28): ");
+				day = sc.nextInt();
+				while(day < 1 || day > 28)
+				{
+						System.err.println("Input is out of range");
+						System.out.print("Day(1-28): ");
+						day = sc.nextInt();
+				}
+			}else {
+				System.out.print("Day(1-29): ");
+				day = sc.nextInt();
+				while(day < 1 || day > 29)
+				{
+						System.err.println("Input is out of range");
+						System.out.print("Day(1-29): ");
+						day = sc.nextInt();
+				}
 			}
-		}else if(month = (1||3||5||7||8||10||12))
+		}
+		
+		if(month == 1||month == 3||month == 5||month == 7||month == 8||month == 10||month == 12)
 		{
 			System.out.print("Day(1-31: ");
 			day = sc.nextInt();
-			while(day < 1 || day > 31
+			while(day < 1 || day > 31)
 			{
 					System.err.println("Input is out of range");
 					System.out.print("Day(1-31): ");
 					day = sc.nextInt();
 			}
-		}else	{
+		}
+		
+		if(month == 4||month == 6||month == 9||month == 11)
+		{
 			System.out.print("Day(1-30: ");
 			day = sc.nextInt();
 			while(day < 1 || day > 30)
@@ -97,15 +119,90 @@ public class CreateTask {
 		return year;
 	}
 	
-	public int setDate(int year, int month, int day)
+	public boolean isLeapYear(int year)
 	{
-		Calender cal = Calender.getInstance();
+		if(year % 4 == 0) 
+			return true;
+		else
+			return false;
+	}
+	
+	public int getHour()
+	{
+		System.out.print("Hour(0-23): ");
+		hour = sc.nextInt();
+		while(hour < 0 || hour > 23)
+		{
+			System.err.println("Input is not within the range");
+			System.out.print("Hour(0-23): ");
+			hour = sc.nextInt();
+		}
+		
+		return hour;
+	}
+	
+	public int getMinute()
+	{
+		System.out.print("Minute(0-59): ");
+		minute = sc.nextInt();
+		while(minute < 0 || minute > 59)
+		{
+			System.err.println("Input is not within the range");
+			System.out.print("Minute(0-23): ");
+			minute = sc.nextInt();
+		}
+		
+		return minute;
+	}
+	
+	public void setDate(int year, int month, int day)
+	{
+		Calendar cal = Calendar.getInstance();
 		cal.set(year, month, day);
 		Date taskDate = cal.getTime();
 		date = sdf.format(taskDate);
 	}
+	
+	public void setTime(int hour, int minute)
+	{
+		Calendar cal = Calendar.getInstance();
+		cal.set(hour, minute);
+		Date taskTime = cal.getTime();
+		timeDue = sdf2.format(taskTime);
+	}
+	
+	protected boolean RepeatTask()
+	{
+		return isRepeating;
+	}
+		
+	protected boolean CheckCompletion()
+	{
+		String temp;
+		if(!isCompleted){
+			return isCompleted = true;
+		}else{
+			
+			System.out.println("Would you like to mark the task as incomplete? (Y/N)");
+			temp = sc.next();
+			
+			while(!temp.equals("Y") || !temp.equals("N"))
+			{
+				System.out.println("Please enter Y or N");
+				temp = sc.next();
+			}
+			
+			if(temp.equals("Y")){
+				return isCompleted = false;
+			}else{
+				return isCompleted = true;
+			}
+		}
+	}
 
 }
      
+        
+ 
         
  
