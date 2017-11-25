0545d0579c4258963cfdc090329a41d4957bea45
Keith Ng <climbr39696@gmail.com>
2017-11-25 02:11:33 -0600

    Updated List.java

--- src/groupProject/List.java
+++ src/groupProject/List.java
@@ -5,16 +5,16 @@ import java.io.File;
 
 public class List {
 	
-	Scanner sc；
+	Scanner sc;
 	private ArrayList<Task> taskList;
 	
-	public List()
+	protected List()
 	{
 		sc = new Scanner(System.in);
 		taskList = new ArrayList<Task>();
 	}
 	
-	public int listFunction()
+	protected int listFunction()
 	{
 		int op;
 		while(op != 5){
@@ -40,12 +40,12 @@ public class List {
 		return 0;
 	}
 	
-	private static void print(String msg)
+	protected static void print(String msg)
 	{
 		System.out.println(msg);
 	}
 
-	private void printTask()
+	protected void printTask()
 	{
 
 		String output;
@@ -56,12 +56,11 @@ public class List {
 			for(int i = 1; i <= taskList.size(); i++){
 				output = i + ". " + taskList.get(i-1).printDescription() + " DUE: " + taskList.get(i-1).printDate() + " STATUS: " + taskList.get(i-1).printStatus();
 				print(output);
-				print("");
 			}
 		}
 	}
 	
-	private void createTask()
+	protected void createTask()
 	{
 		Task task = new Task();
 		task.getDescription();
@@ -78,7 +77,7 @@ public class List {
 		print("TASK CREATED");
 	}
 	
-	private int editTask()
+	protected int editTask()
 	{
 		System.out.print("INDEX OF THE TASK YOU WISH TO EDIT: ");
 		int i = sc.nextInt();
@@ -99,7 +98,7 @@ public class List {
 			case 1: task.setTime(); break;
 			case 2: task.repeatTask(); break;
 			case 3: task.getNote(); break;
-			case 4: //create subtask
+			case 4: task.addSubtask(); break;
 			case 5: break;
 			default: print("INVALID INPUT NUMBER");
 			}
@@ -108,12 +107,22 @@ public class List {
 		return 0;
 	}
 	
-	private void removeTask()
+	protected void removeTask()
 	{
 		System.out.print("INDEX OF THE TASK YOU WISH TO REMOVE: ");
 		int i = sc.nextInt();
-		taskList.remove(i-1);
-		print("TASK REMOVED");
+		
+		try {
+			taskList.remove(i-1);
+			print("TASK REMOVED");
+		
+			//sort remaining tasks;
+			for(int x = i-1; x <= taskList.size(); x++){
+				taskList.set(x, taskList.get(x+1));
+			}
+		} catch (IndexOutOfBoundsException e) {
+			print("INDEX IS OUT OF RANGE");
+		}
 	}
 
 	
